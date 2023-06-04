8098b070ba7abedacb1c118d27ce454ea165f26c
ElPresidentePoole <gpoole@posteo.net>
2023-06-04 01:11:24 -0400

    pop

--- main.c
+++ main.c
@@ -12,6 +12,9 @@ int main() {
     const int friend_original_height = friend.height;
     const int friend_original_y = screenHeight/2 - friend.height/2;
 
+    InitAudioDevice();
+    Sound pop = LoadSound("./click.ogg");
+
     int friend_y = friend_original_y;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -23,6 +26,7 @@ int main() {
         // Update
         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             times_pet++;
+            PlaySound(pop);
             friend.width = friend_original_width * 1;
             friend.height = friend_original_height * 0.6;
             friend_y += 75;
